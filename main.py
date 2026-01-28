f0a3cf39ccc1defff4e52798762e9bb8b49d96ab
AD-TECOTEC\suzuki.kazuki <suzuki.kazuki@tecotec.co.jp>
2026-01-28 15:09:22 +0900

    no message

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import requests
 
-# area = input()
+area = input("地域名を入力してください:")
 
-response = requests.get("https://www.jma.go.jp/bosai/forecast/data/forecast/130000.json")
+response = requests.get("https://www.jma.go.jp/bosai/common/const/area.json")
 
 data = response.json()
+area_code = None
 
-weather = data[0]["timeSeries"][0]["areas"][0]["weathers"][1]
+for code, areaname in data["offices"].items():
+    if areaname["name"] == area:
+        area_code = code
+        break
 
-maxtemps = data[1]["timeSeries"][1]["areas"][0]["tempsMax"][1]
+if area_code == None:
+    print("地域が見つかりません")
+    exit()
+
+areaData = requests.get(f"https://www.jma.go.jp/bosai/forecast/data/forecast/{area_code}.json").json()
+
+weather = areaData[0]["timeSeries"][0]["areas"][0]["weathers"][1]
+
+maxtemps = areaData[1]["timeSeries"][1]["areas"][0]["tempsMax"][1]
 
 print("明日の天気は" + weather + "、最高気温は" + maxtemps + "℃です。")
