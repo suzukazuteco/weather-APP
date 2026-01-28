0019deac6cb2cd7255a024d40354d603b85261da
AD-TECOTEC\suzuki.kazuki <suzuki.kazuki@tecotec.co.jp>
2026-01-28 13:11:29 +0900

    no message

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import requests
 
-area = input()
+# area = input()
 
-response = requests.get("https://www.jma.go.jp/bosai/common/const/area.json")
+response = requests.get("https://www.jma.go.jp/bosai/forecast/data/forecast/130000.json")
+
 data = response.json()
 
-# print(data)
+weather = data[0]["timeSeries"][0]["areas"][0]["weathers"][1]
+
+maxtemps = data[1][]
+
+print(weather)
