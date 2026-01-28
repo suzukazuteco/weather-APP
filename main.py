9ba45a5e63cd29e7c8b911f2a0683eebe6766039
AD-TECOTEC\suzuki.kazuki <suzuki.kazuki@tecotec.co.jp>
2026-01-28 13:41:41 +0900

    no message

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ data = response.json()
 
 weather = data[0]["timeSeries"][0]["areas"][0]["weathers"][1]
 
-maxtemps = data[1][]
+maxtemps = data[1]["timeSeries"][1]["areas"][0]["tempsMax"][1]
 
-print(weather)
+print("明日の天気は" + weather + "、最高気温は" + maxtemps + "℃です。")
