b2f84b8c3b8c0f8f0c26faef486e8581c88ce063
AD-TECOTEC\yoshizawa.kazuki <yoshizawa.kazuki@tecotec.co.jp>
2026-02-12 14:13:21 +0900

    no message

--- main.py
+++ main.py
@@ -1,26 +1,15 @@
-import requests
-import AreaCodeData
+from typing import Union
 
-area = input("地域名を入力してください:")
+from fastapi import FastAPI
 
-response = requests.get("https://www.jma.go.jp/bosai/common/const/area.json")
+app = FastAPI()
 
-data = response.json()
-area_code = None
 
-for code, areaname in data["offices"].items():
-    if areaname["name"] == area:
-        area_code = code
-        break
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
-if area_code == None:
-    print("地域が見つかりません")
-    exit()
 
-areaData = requests.get(f"https://www.jma.go.jp/bosai/forecast/data/forecast/{area_code}.json").json()
-
-weather = areaData[0]["timeSeries"][0]["areas"][0]["weathers"][1]
-
-maxtemps = areaData[1]["timeSeries"][1]["areas"][0]["tempsMax"][1]
-
-print("明日の天気は" + weather + "、最高気温は" + maxtemps + "℃です。")
+@app.get("/{areacode}")
+def read_item(areacode):
+    return f"{arecode}の天気は～～です"
